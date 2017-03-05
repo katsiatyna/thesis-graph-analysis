5eda5a748ee59b49956d75d60554950cc1bc595e
katsiaryna <katherine.krasnoschok@hotmail.com>
2017-03-06 00:13:05 +0100

    Imports for identifying peaks added. Check later

--- upc/bsc/kde/tests/PyQTFitTest.py
+++ upc/bsc/kde/tests/PyQTFitTest.py
@@ -1,13 +1,15 @@
 import numpy as np
-from scipy.stats import norm
+#from scipy.stats import norm
 from matplotlib import pylab as plt
 from pyqt_fit import kde
+from detect_peaks import detect_peaks
+import peakutils
 
 
-f = norm(loc=0, scale=1)
-x = f.rvs(500)
-xs = np.r_[-3:3:1024j]
-ys = f.pdf(xs)
+#f = norm(loc=0, scale=1)
+#x = f.rvs(500)
+#xs = np.r_[-3:3:1024j]
+#ys = f.pdf(xs)
 #h = plt.hist(x, bins=30, normed=True, color=(0,.5,0,1), label='Histogram')
 #plt.plot(xs, ys, 'r--', linewidth=2, label='$\mathcal{N}(0,1)$')
 #plt.xlim(-3,3)
@@ -36,7 +38,7 @@ print len(ds) == len(estimation)
 plt.plot(ds, est(ds), label='Estimate (bw={:.3g})'.format(est.bandwidth))
 plt.legend(loc='best')
 plt.show()
-
+ #use peaksutils or
 # f = norm(loc=0, scale=1)
 # xs = np.r_[-3:3:1024j]
 # nbins = 20
